79eb7b6068d917ccdc6c57bdd19e6e8939020121
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-06-25 14:31:32 +0530

    Changes in tokens.js

--- tokens.js
+++ tokens.js
@@ -25,40 +25,55 @@ module.exports.generateToken = function (data,cryptoSecret,HMACSecret) {
 
     //Creating HMAC KEY
     var payloadHMAC = crypto.createHmac('sha1', HMACSecret).update(dJSON).digest('base64');
+
     var payload = dJSON +"|||"+ payloadHMAC;
     var payload64 = new Buffer(payload).toString('base64');
+
     var cryptextObj = crypto.createCipher('aes192', cryptoSecret);
     var cryptoUpdate = cryptextObj.update(payload64,'base64','base64');
     var cryptext = cryptoUpdate + cryptextObj.final('base64');
 
-    return cryptext;
+    var rawCryptext = cryptext +"."+ data.userId+"."+crypto.createHmac('sha1', HMACSecret).update(cryptext).digest('base64');
 
+    return new Buffer(rawCryptext).toString('base64');
 };
 
 module.exports.parseToken = function (cryptext,cryptoSecret,HMACSecret) {
 
-    var decryptObj = crypto.createDecipher('aes192', cryptoSecret);
-    var decrytUpdate = decryptObj.update(cryptext,'base64','base64');
-    var decrypttext =decrytUpdate + decryptObj.final('base64');
-    var newpayload = new Buffer(decrypttext,'base64').toString('utf8').split('|||');
+    //console.log(cryptext);
+    var UNDOrawBase64 = new Buffer(cryptext,'base64').toString('utf8');
+    var raw = UNDOrawBase64.split('.');
 
-    //Checking HMAC VALUE
-    var decrypttext;
-    if(newpayload[1]==crypto.createHmac('sha1', HMACSecret).update(newpayload[0]).digest('base64'))
-    {
-        decrypttext = JSON.parse(newpayload[0]);
+    console.log("User-ID: " + raw[1]);
+    //console.log("digest: " + raw[2]);
+    //console.log("digest: "+crypto.createHmac('sha1', HMACSecret).update(raw[0]).digest('base64'));
 
-        var tokenGenerateDateTime = new Date(decrypttext.expiry);
-        var currentDateTime = new Date();
+    if(raw[2]==crypto.createHmac('sha1', HMACSecret).update(raw[0]).digest('base64')) {
 
+        var decryptObj = crypto.createDecipher('aes192', cryptoSecret);
+        var decrytUpdate = decryptObj.update(raw[0], 'base64', 'base64');
+        var decrypttext = decrytUpdate + decryptObj.final('base64');
+        var newpayload = new Buffer(decrypttext, 'base64').toString('utf8').split('|||');
 
-        if( tokenGenerateDateTime <= currentDateTime){
-            throw this.InvalidTokenError();
+        //Checking HMAC VALUE
+        var decrypttext;
+        if (newpayload[1] == crypto.createHmac('sha1', HMACSecret).update(newpayload[0]).digest('base64')) {
+            decrypttext = JSON.parse(newpayload[0]);
+
+            var tokenGenerateDateTime = new Date(decrypttext.expiry);
+            var currentDateTime = new Date();
+
+            if (tokenGenerateDateTime <= currentDateTime) {
+                throw this.InvalidTokenError();
+            }
+        }
+        else {
+            throw "Token Temper Exception";
         }
     }
     else
     {
-       throw "Token Temper Exception";
+        throw this.InvalidTokenError();
     }
     return decrypttext;
 };
